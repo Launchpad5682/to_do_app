4c3aef8dcc477f2b32833070acbe395c3d837ba1
launchpad5682 <launchpad5682@gmail.com>
2021-05-21 17:17:07 +0530

    Input component added

---
+++ src/components/input.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class InputBox extends React.Component {
+  render() {
+    return (
+      <div>
+          <input></input>
+      </div>
+    );
+  }
+}
