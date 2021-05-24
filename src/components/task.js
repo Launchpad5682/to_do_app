b98f253511c3c0a1b559c29218ffcdf56bc1f74a
launchpad5682 <launchpad5682@gmail.com>
2021-05-24 22:05:32 +0530

    added newly created tasks to the list and automatic rendering

--- src/components/task.js
+++ src/components/task.js
@@ -3,32 +3,11 @@ import CheckBox from "./checkbox";
 import DeleteIcon from "./deleteIcon";
 // delete the tasks which are completed
 
-export default function Task() {
-  const [loading, setLoading] = useState(true);
-  const [tasks, setTasks] = useState([]);
-  const [done, setDone] = useState(true);
-
-  //onChange={handleCheck}
-
-  useEffect(() => {
-    fetch("http://localhost:5555/tasks")
-      .then((task) => task.json())
-      .then((data) => setTasks([...data]))
-      .then(setLoading(false));
-  }, []);
-
+export default function Task(props) {
   return (
     <dir className="task">
-      {loading || !tasks ? (
-        <div>loading....</div>
-      ) : (
-        tasks.map((task) => (
-          <div key={task["_id"]["$oid"]}>
-            <CheckBox />
-            {task.task} <DeleteIcon />
-          </div>
-        ))
-      )}
+      <CheckBox />
+      {props.items.task} <DeleteIcon />
     </dir>
   );
 }
