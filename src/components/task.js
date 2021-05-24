4463462a5aa6b8b1e13f9e2fa7e3174c90d53262
launchpad5682 <launchpad5682@gmail.com>
2021-05-24 13:05:53 +0530

    updated the code with hooks and fetched data

--- src/components/task.js
+++ src/components/task.js
@@ -1,38 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // delete the tasks which are completed
-export default class Task extends React.Component {
-  state = {
-    loading: true,
-    tasks: null,
-    done: true,
-  };
 
+export default function Task() {
+  const [loading, setLoading] = useState(true);
+  const [tasks, setTasks] = useState([]);
+  const [done, setDone] = useState(true);
+
+  //onChange={handleCheck}
+
+  useEffect(() => {
+    fetch("http://localhost:5555/tasks")
+      .then((task) => task.json())
+      .then((data) => setTasks([...data]))
+      .then(setLoading(false));
+  }, []);
+
+  return (
+    <dir className="task">
+      <input type="checkbox" value={done}></input>
+      {loading || !tasks ? (
+        <div>loading....</div>
+      ) : (
+        tasks.map((task) => <div key={task["_id"]["$oid"]}>{task.task}</div>)
+      )}
+    </dir>
+  );
+}
+
+/* 
   async componentDidMount() {
     const url = "http://localhost:5555/tasks";
     const response = await fetch(url);
     const data = await response.json();
-    //console.log(data[0].task);
-    this.setState({ tasks: data[0].task, loading: false });
+    console.log(data);
+    this.setState({ tasks: data, loading: false });
   }
 
   handleCheck = (event) => {
     this.setState({ done: event.target.checked });
   };
-
-  render() {
-    return (
-      <dir className="task">
-        <input
-          type="checkbox"
-          value={this.state.done}
-          onChange={this.handleCheck}
-        ></input>
-        {this.state.loading || !this.state.tasks ? (
-          <div>loading....</div>
-        ) : (
-          <div>{this.state.tasks}</div>
-        )}
-      </dir>
-    );
-  }
-}
+*/
